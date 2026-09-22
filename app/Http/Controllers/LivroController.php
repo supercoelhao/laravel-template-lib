9131083188eb51dde29b3bb8f4c5d90b35ac905d
supercoelhao <fernandoazevedogeek@gmail.com>
2026-09-21 22:01:15 -0300

    Update LivroController.php

--- app/Http/Controllers/LivroController.php
+++ app/Http/Controllers/LivroController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Livro;
+
 class LivroController extends Controller
 {
     /**
@@ -13,7 +15,7 @@ class LivroController extends Controller
      */
     public function index()
     {
-        $livros = [];
+        $livros = livro::all();
         return view('livros.index', ['livros' => $livros]);
     }
 
